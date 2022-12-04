79f8537a2016a8e0b25a3980791b3a0cfd252420
audi0652 <113797010+dev0652@users.noreply.github.com>
2022-12-04 14:29:06 +0200

    Finished adaptive styles

--- js/modal.js
+++ js/modal.js
@@ -5,7 +5,7 @@
 		openModalBtn: document.querySelector("[data-modal-open]"),
 		closeModalBtn: document.querySelector("[data-modal-close]"),
 		modal: document.querySelector("[data-modal]"),
-		bodyScrollLock: document.querySelector("body"),
+		// bodyScrollLock: document.querySelector("body"),
 	};
 
 	refs.openModalBtn.addEventListener("click", toggleModal);
@@ -13,7 +13,7 @@
 
 	function toggleModal() {
 		refs.modal.classList.toggle("is-hidden");
-		refs.bodyScrollLock.classList.toggle("scroll-locked");
+		// refs.bodyScrollLock.classList.toggle("scroll-locked");
 	}
 })();
 
