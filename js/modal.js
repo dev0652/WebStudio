c8a499c76097950c304db2c3844179fc9ca499e3
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-11-02 02:58:56 +0200

    fixes indentation

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,20 @@
 // Modal window
 
 (() => {
-	const refs = {
-		openModalBtn: document.querySelector("[data-modal-open]"),
-		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
-		body: document.querySelector("body"),
-	};
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
+  };
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-	function toggleModal() {
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
 
-		refs.modal.classList.toggle("is-hidden");
-
-		refs.body.classList.toggle("scroll-locked");
-		// https://www.youtube.com/watch?v=Z5lYspOgtrY&t=477s
-	}
+    refs.body.classList.toggle('scroll-locked');
+    // https://www.youtube.com/watch?v=Z5lYspOgtrY&t=477s
+  }
 })();
-
-
